a9320c381bd543709f58bf91bc1064af233e6e78
nictes1 <nikolastesone@gmail.com>
2023-08-26 00:50:39 -0300

    client web basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [rows, setRows] = useState(5);
+  const [cols, setCols] = useState(5);
+  const [matrix, setMatrix] = useState([]);
+
+  const generateMatrix = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/spiral?rows=${rows}&cols=${cols}`);
+      const data = await response.json();
+      const spiralMatrix = data.rows;
+
+      setMatrix(spiralMatrix);
+      
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const renderMatrix = () => {
+    return (
+      <div className="matrix-container">
+        {matrix.map((row, rowIndex) => (
+          
+          <div key={rowIndex} className="matrix-row">
+            
+            {row.map((cell, colIndex) => (
+              
+              <span key={colIndex} className="matrix-cell">
+                {cell}
+              </span>
+            
+            ))}
+            <br /> {}
+          
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Fibonacci Spiral </h1>
+      <div className="input-container">
+        <label className="input-row" htmlFor="rows">Number of Rows:</label>
+        <input
+          className="number-row"
+          type="number"
+          id="rows"
+          value={rows}
+          onChange={(e) => setRows(parseInt(e.target.value))}
+        />
+        <label className="input-col" htmlFor="cols">Number of Columns:</label>
+        <input
+          className="number-col"
+          type="number"
+          id="cols"
+          value={cols}
+          onChange={(e) => setCols(parseInt(e.target.value))}
+        />
+        <button type="button" onClick={generateMatrix}>
+          Calculate
+        </button>
+      </div>
+      <div className="matrix-container">{renderMatrix()}</div>
     </div>
   );
 }
